7e75dd3c019093d27ac5155be6ad8ecf412a0e5d
zannatwebsolution <zannatwebsolution@gmail.com>
2022-10-11 21:56:44 +0600

    Create Error Page

---
+++ src/components/ErrorPage/ErrorPage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const ErrorPage = () => {
+    return (
+        <div>
+            <h2>Page Not Found.</h2>
+            <p>404 Error</p>
+        </div>
+    );
+};
+
+export default ErrorPage;
